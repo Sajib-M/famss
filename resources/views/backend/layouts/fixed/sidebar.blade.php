f312a61418b32ec89d884e169001d9d2f1bae099
Maksud Bhuiyan <maksudbhuiyan595>
2023-12-08 16:04:38 +0600

    backend init

---
+++ resources/views/backend/layouts/fixed/sidebar.blade.php
@@ -0,0 +1,28 @@
+<aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link " href="#">
+          <i class="bi bi-grid"></i>
+          <span>Dashboard</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link collapsed"  href="#">
+          <i class="bi bi-menu-button-wide"></i><span>Categories</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link collapsed"  href="#">
+          <i class="bi bi-menu-button-wide"></i><span>Products</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link collapsed"  href="#">
+          <i class="bi bi-menu-button-wide"></i><span>Vendors</span>
+        </a>
+      </li>
+      </ul>
+</aside>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Backend\HomeController;
 use Illuminate\Support\Facades\Route;
 
 
